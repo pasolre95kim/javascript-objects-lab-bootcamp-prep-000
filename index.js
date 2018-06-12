f4ee11fe1e4a583e12bbb0bbe8ac4f49625fbbfa
Michelle Kim <pasolre95kim@gmail.com>
2018-06-12 18:55:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return obj
 }
 
-function updateObjectWithKeyAndValue(obj,key,value) {
+function updateObjectWithKeyAndValue(object,key,value) {
   object[key] = value
   return Object.assign({}, object, { [key]: value})
 }
