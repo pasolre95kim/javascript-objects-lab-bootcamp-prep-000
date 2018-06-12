50f370a54a1737b09c1ec71e8254cf765cf1e8a3
Michelle Kim <pasolre95kim@gmail.com>
2018-06-12 19:11:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,5 +21,10 @@ function deleteFromObjectByKey(object,key) {
 }
 
 function deleteFromObjectByKey(object,key) {
+  var obj = { foo: 'bar' }
   var newObj = Object.assign({}, obj)
+  newObj
+  delete newObj.foo
+  newObj
+  obj 
 }
