03b863dab30a7b188b617c18cbf1f5ccc530723f
Michelle Kim <pasolre95kim@gmail.com>
2018-06-12 18:50:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = new Object()
 
 function updateObjectWithKeyAndValue(obj, key, value) {
-  object[key] = value
+  obj[key] = value
 
-  return Object.assign({}, object, {[key=value]})
+  return obj
 }
